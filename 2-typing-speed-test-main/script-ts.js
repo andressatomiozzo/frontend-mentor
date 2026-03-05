910fb8ebf7c81c6ad65af194758daf43cc566177
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-05 09:10:37 -0400

    Typing speed test - The time (in minutes), words per minute (WPM), and accuracy are being calculated and displayed on the screen.

--- 2-typing-speed-test-main/script-ts.js
+++ 2-typing-speed-test-main/script-ts.js
@@ -3,12 +3,17 @@ const message = document.querySelector("#message");
 const textInput = document.querySelector("#textInput");
 const correctCount = document.querySelector("#correctCount");
 const errorCount = document.querySelector("#errorCount");
-if (!message || !textInput || !correctCount || !errorCount)
+const timeContainer = document.querySelector("#time-container");
+const wpmContainer = document.querySelector("#wpm-container");
+const accuracyContainer = document.querySelector("#accuracy-container");
+if (!message || !textInput || !correctCount || !errorCount || !timeContainer || !accuracyContainer || !wpmContainer)
     throw new Error("Some element is not linked to the DOM.");
+//Se não tiver uma mensagem como desafio ele solta um erro
 if (message.textContent.length === 0) {
     console.log("ops, não tem messagem");
 }
 else {
+    //Quebra a mensagem em pequenos spans
     const messageBroaken = message.textContent.split("");
     message.innerHTML = "";
     messageBroaken.forEach((character) => {
@@ -17,23 +22,32 @@ else {
         spanCharacter.classList.add("message-span");
         message.appendChild(spanCharacter);
     });
+    //Pega todos os spans
     const messageSpans = document.querySelectorAll(".message-span");
+    const totalLenght = messageSpans.length;
     let errorTotal = 0;
+    //Para começar o tempo
     let started = false;
     let startTime = 0;
     let endTime = 0;
     textInput.addEventListener("input", () => {
         if (started === false) {
+            //Na primeira letra digitada cai aqui e marca o tempo de início
             startTime = Date.now();
             started = true;
         }
-        // console.log(startTime);
         let correct = 0;
         let error = 0;
+        //Mostra o tempo real
+        let nowTimeSeconds = Math.floor((Date.now() - startTime) / 1000);
+        let nowTimeMinutes = Math.floor(nowTimeSeconds / 60) + (nowTimeSeconds % 60) * 0.01;
+        let wpmNow = Math.floor(nowTimeMinutes === 0 ? 0 : totalLenght / 5 / nowTimeMinutes);
+        timeContainer.innerText = `${"tempo: " + nowTimeMinutes + " wpm:" + wpmNow}`;
+        // Remove todas as classes de tudo, pois quando entrar no for ele vai adicionar
         messageSpans.forEach((span) => span.classList.remove("active", "correct", "incorrect"));
-        let nowTime = (Date.now() - startTime) / 1000;
+        // Pra cada caractere que está e já foi adicionado ele verifica se está correto ou não comparando os spans
         for (let i = 0; i < textInput.value.length; i++) {
-            if (i !== messageSpans.length) {
+            if (i !== totalLenght) {
                 if (textInput.value[i] === messageBroaken[i]) {
                     messageSpans[i].classList.add("correct");
                     correct++;
@@ -41,23 +55,30 @@ else {
                 else {
                     messageSpans[i].classList.add("incorrect");
                     error++;
-                    // --------------------------- Isso aqui foi incrível ------------------
+                    //// --------------------------- Isso aqui foi incrível ------------------
                     i === textInput.value.length - 1 ? (errorTotal = errorTotal + 1) : errorTotal;
-                    // ---------------------------------------------------------------------
+                    //// ---------------------------------------------------------------------
                 }
-                if (i === textInput.value.length - 1 && i !== messageSpans.length - 1) {
+                // Atualizar cursor
+                if (i === textInput.value.length - 1 && i !== totalLenght - 1) {
                     messageSpans[i + 1].classList.add("active");
                 }
+                //Accuracy
+                let accuracy = Math.floor(((totalLenght - errorTotal) / totalLenght) * 100);
+                accuracyContainer.innerText = `Accuracy: ${accuracy < 0 ? 0 : accuracy}%`;
             }
             else {
                 console.log("You have completed the exercise.");
                 textInput.remove();
                 endTime = Date.now();
-                let elapsedTime = (endTime - startTime) / 1000;
-                console.log(`Time: ${elapsedTime}`);
+                let elapsedTimeSeconds = Math.floor((endTime - startTime) / 1000);
+                let elapsedTimeMinutes = Math.floor(elapsedTimeSeconds / 60) + (elapsedTimeSeconds % 60) * 0.01;
+                let wpmTotal = elapsedTimeMinutes === 0 ? 0 : totalLenght / 5 / elapsedTimeMinutes;
+                timeContainer.innerText = `Final time: ${elapsedTimeMinutes}`;
             }
         }
-        correctCount.innerText = `Acertos: ${correct}`;
-        errorCount.innerText = `Erros: ${error} Erros totais: ${errorTotal}`;
+        //Mostra na tela o que foi feito
+        correctCount.innerText = `Number of correct characters:: ${correct}`;
+        errorCount.innerText = `Number of incorrect characters:: ${error} Total number of incorrect characters:: ${errorTotal}`;
     });
 }
