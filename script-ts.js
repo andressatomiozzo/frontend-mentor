441df36c75b49fc8abfacec8f6edb7708d458209
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-09 15:06:43 -0400

    Typing speed test - debugging

--- script-ts.js
+++ script-ts.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 const message = document.querySelector("#message");
 const restartBtn = document.querySelector(".restart");
 const textInput = document.querySelector("#textInput");
@@ -68,16 +59,16 @@ const breakMessage = () => {
         reset();
     }
 };
-const levelArray = ["easy", "medium", "hard"]; // ("easy" | "medium" | "hard") -> os únicos valores aceitos são estes
-const randomLevel = levelArray[Math.floor(Math.random() * 3)]; // Sortear um dos índices
 // Puxar os dados do data.json pra esse arquivo
-const pullData = () => __awaiter(void 0, void 0, void 0, function* () {
+const pullData = async () => {
+    const levelArray = ["easy", "medium", "hard"]; // ("easy" | "medium" | "hard") -> os únicos valores aceitos são estes
+    const randomLevel = levelArray[Math.floor(Math.random() * 3)]; // Sortear um dos índices
     try {
-        const data = yield fetch("data.json");
+        const data = await fetch("data.json");
         if (!data.ok) {
             throw new Error("Ops! Something is wrong!");
         }
-        const dataJson = (yield data.json());
+        const dataJson = (await data.json());
         const chosenLevel = dataJson[randomLevel];
         const chosenIndex = Math.floor(Math.random() * chosenLevel.length);
         const chosenText = chosenLevel[chosenIndex];
@@ -87,7 +78,7 @@ const pullData = () => __awaiter(void 0, void 0, void 0, function* () {
     catch (err) {
         console.log(err);
     }
-});
+};
 // A cada letra teclada no input vai rodar isso aqui:
 textInput.addEventListener("input", () => {
     if (started === false) {
@@ -99,43 +90,49 @@ textInput.addEventListener("input", () => {
     correct = 0;
     error = 0;
     // Mostra o tempo real
-    let nowTimeSeconds = Math.floor((Date.now() - startTime) / 1000);
-    let nowTimeMinutes = Math.floor(nowTimeSeconds / 60) + (nowTimeSeconds % 60) * 0.01;
-    let wpmNow = Math.floor(nowTimeMinutes === 0 ? 0 : totalLenght / 5 / nowTimeMinutes);
-    timeContainer.innerText = `Tempo: ${nowTimeMinutes}`;
+    let nowTimeTotalSeconds = Math.floor((Date.now() - startTime) / 1000);
+    let nowTimeMinutes = nowTimeTotalSeconds / 60;
+    let nowTimeSeconds = nowTimeTotalSeconds % 60;
+    let nowTimeSecondsString = `${nowTimeSeconds}`.padStart(2, "0");
+    let nowTimeString = `${Math.floor(nowTimeMinutes)}:${nowTimeSecondsString}`;
+    console.log(nowTimeString);
+    let wpmNow = Math.floor(nowTimeMinutes === 0 ? 0 : textInput.value.length / 5 / nowTimeMinutes);
+    timeContainer.innerText = `Tempo: ${nowTimeString}`;
     wpmContainer.innerText = `WPM: ${wpmNow}`;
     // Remove todas as classes de tudo, pois quando entrar no for ele vai adicionar
     messageSpans.forEach((span) => span.classList.remove("active", "correct", "incorrect"));
+    if (textInput.value.length === 0) {
+        messageSpans[0].classList.add("active");
+    }
     // Pra cada caractere que está e já foi adicionado ele verifica se está correto ou não comparando os spans
     for (let i = 0; i < textInput.value.length; i++) {
-        if (i !== totalLenght) {
-            if (textInput.value[i] === messageBroaken[i]) {
-                messageSpans[i].classList.add("correct");
-                correct++;
-            }
-            else {
-                messageSpans[i].classList.add("incorrect");
-                error++;
-                if (!errorTotal.includes(i))
-                    errorTotal.push(i);
-            }
-            // Atualizar cursor
-            if (i === textInput.value.length - 1 && i !== totalLenght - 1) {
-                messageSpans[i + 1].classList.add("active");
-            }
-            //Accuracy
-            let accuracy = Math.floor(((totalLenght - errorTotal.length) / totalLenght) * 100);
-            accuracyContainer.innerText = `Accuracy: ${accuracy < 0 ? 0 : accuracy}%`;
+        if (textInput.value[i] === messageBroaken[i]) {
+            messageSpans[i].classList.add("correct");
+            correct++;
         }
         else {
-            console.log("You have completed the exercise.");
-            textInput.remove();
-            endTime = Date.now();
-            let elapsedTimeSeconds = Math.floor((endTime - startTime) / 1000);
-            let elapsedTimeMinutes = Math.floor(elapsedTimeSeconds / 60) + (elapsedTimeSeconds % 60) * 0.01;
-            let wpmTotal = elapsedTimeMinutes === 0 ? 0 : totalLenght / 5 / elapsedTimeMinutes;
-            timeContainer.innerText = `Final time: ${elapsedTimeMinutes}`;
+            messageSpans[i].classList.add("incorrect");
+            error++;
+            if (!errorTotal.includes(i))
+                errorTotal.push(i);
         }
+        // Atualizar cursor
+        if (i === textInput.value.length - 1 && i !== totalLenght - 1) {
+            messageSpans[i + 1].classList.add("active");
+        }
+        // Accuracy
+        let accuracy = Math.floor(((totalLenght - errorTotal.length) / totalLenght) * 100);
+        accuracyContainer.innerText = `Accuracy: ${accuracy < 0 ? 0 : accuracy}%`;
+    }
+    // Acabou o exercício
+    if (textInput.value.length === totalLenght) {
+        console.log("You have completed the exercise.");
+        textInput.value = "";
+        endTime = Date.now();
+        let elapsedTimeSeconds = Math.floor((endTime - startTime) / 1000);
+        let elapsedTimeMinutes = Math.floor(elapsedTimeSeconds / 60) + (elapsedTimeSeconds % 60) * 0.01;
+        let wpmTotal = elapsedTimeMinutes === 0 ? 0 : totalLenght / 5 / elapsedTimeMinutes;
+        timeContainer.innerText = `Final time: ${elapsedTimeMinutes}`;
     }
     //Mostra na tela o que foi feito sempre atualizando os dados
     correctCount.innerText = `Number of correct characters:: ${correct}`;
