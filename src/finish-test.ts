04031ae5afc87a2f26e8acc622d8d7f685fed474
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-18 17:29:55 -0400

    Test commit

--- src/finish-test.ts
+++ src/finish-test.ts
@@ -63,6 +63,8 @@ export const finishTest = () => {
       });
     }
 
+
+    
     showResults(wpmContainer, accuracyContainer, correctCharactersContainer, incorrectCharactersContainer);
   });
   if (bestWPM === null) {
